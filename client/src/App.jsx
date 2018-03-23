9aec43c520dbe9824666b64e8f3096201d40cdf8
Levy Medina II <levymedina3@gmail.com>
2018-03-24 02:45:49 +0800

    added front end

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      test: 'App'
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/users')
+      .then(res => {
+        console.log(res)
+        this.setState({ test: res.data })
+      }).catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        { this.state.test }
+      </div>
+    )
+  }
+}
+
+export default App
