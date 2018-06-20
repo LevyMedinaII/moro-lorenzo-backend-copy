491e5c5988ce7d23a228c1e977354f95864be7c1
Levy Medina II <levymedina3@gmail.com>
2018-06-20 22:22:39 +0800

    initial commit

--- Code Busters/js/clock.js
+++
@@ -1,9 +0,0 @@
-function logout() {
-    var req = new XMLHttpRequest();
-    req.open("POST", "http://moro-system.herokuapp.com/admins/login", true);
-    req.withCredentials = true;
-    req.send();
-    hide_error();
-    location.href="index.html";
-
-  }
